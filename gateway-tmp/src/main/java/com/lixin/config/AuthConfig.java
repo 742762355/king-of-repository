099eeea9905dce4058562bac7afa50459f5db72c
742762355 <742762355@qq.com>
2021-09-28 14:56:12 +0800

    自动装配权限验证

---
+++ gateway-tmp/src/main/java/com/lixin/config/AuthConfig.java
@@ -0,0 +1,18 @@
+package com.lixin.config;
+
+
+//import com.lixin.annotation.EnableAuthorization;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+//@EnableAuthorization
+public class AuthConfig {
+
+
+    @PostConstruct
+    public void init(){
+        System.out.println("配置初始化!!!");
+    }
+}
---
+++ gateway-tmp/src/main/java/com/lixin/interceptor/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.lixin.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowCredentials(true);
+        cfg.addAllowedOrigin("*");
+        cfg.addAllowedMethod("*");
+        cfg.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+        return new CorsWebFilter(source);
+    }
+}
